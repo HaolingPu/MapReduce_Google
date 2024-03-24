ba63c7ba50e7591678f8386a4fb8a0149d08209e
HaolingPu <phling@umich.edu>
2024-03-23 21:47:22 -0400

    shutdown

--- mapreduce/manager/__main__.py
+++ mapreduce/manager/__main__.py
@@ -6,6 +6,7 @@ import json
 import time
 import click
 import mapreduce.utils
+import socket
 
 
 # Configure logging
@@ -17,6 +18,10 @@ class Manager:
 
     def __init__(self, host, port):
         """Construct a Manager instance and start listening for messages."""
+        self.host = host
+        self.port = port
+        self.signals = {"shutdown": False}
+        self.workers = []
 
         LOGGER.info(
             "Starting manager host=%s port=%s pwd=%s",
@@ -24,17 +29,95 @@ class Manager:
         )
 
         # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register",
-            "worker_host": "localhost",
-            "worker_port": 6001,
-        }
-        LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        # message_dict = {
+        #     "message_type": "register",
+        #     "worker_host": "localhost",
+        #     "worker_port": 6001,
+        # }
+        # LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        LOGGER.debug("IMPLEMENT ME!")
-        time.sleep(120)
+        # TODO:
+        self.manager_tcp_server()
+
+
+# phling的code：
+    def manager_tcp_server (self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Bind the socket to the server
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.host, self.port))
+            sock.listen()
+            sock.settimeout(1)
+
+            LOGGER.info("TCP Server listening on %s:%s", self.host, self.port)
+
+            while not self.signals["shutdown"]:
+                try:
+                    clientsocket, address = sock.accept()
+                except socket.timeout:
+                    continue
+                LOGGER.info("Connection from %s", address[0])
+            # Socket recv() will block for a maximum of 1 second.  If you omit
+            # this, it blocks indefinitely, waiting for packets.
+                clientsocket.settimeout(1)
+                # Receive data, one chunk at a time.  If recv() times out before we
+            # can read a chunk, then go back to the top of the loop and try
+            # again.  When the client closes the connection, recv() returns
+            # empty data, which breaks out of the loop.  We make a simplifying
+            # assumption that the client will always cleanly close the
+            # connection.
+                with clientsocket:
+                    message_chunks = []
+                    while True:    # ????????
+                        try:
+                            data = clientsocket.recv(4096)
+                        except socket.timeout:
+                            continue
+                        if not data:
+                            break
+                        message_chunks.append(data)
+
+                # Decode list-of-byte-strings to UTF8 and parse JSON data
+                message_bytes = b''.join(message_chunks)
+                message_str = message_bytes.decode("utf-8")
+
+                try:
+                    message_dict = json.loads(message_str)
+                except json.JSONDecodeError:
+                    LOGGER.warning("Invalid JSON message received and ignored.")  # 加了为了debug
+                    continue
+
+                LOGGER.info("Received message: %s", message_dict)
+
+                if message_dict["message_type"] == "shutdown" :
+                    # send the shutdown message to all the workers
+                    for worker in self.workers:
+                        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                            # connect to the server
+                            sock.connect((worker.host, worker.port))
+                            # send a message
+                            message = json.dumps({"shutdown": True})
+                            sock.sendall(message.encode('utf-8'))
+
+                    self.signals["shutdown"] = True
+                    LOGGER.info("Manager shut down!")
+                                
+
+
+
+    def manager_tcp_client(self):
+        """Test TCP Socket Client."""
+        # create an INET, STREAMing socket, this is TCP
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+
+            # connect to the server
+            sock.connect(("localhost", 8000))
+
+            # send a message
+            message = json.dumps({"hello": "world"})
+            sock.sendall(message.encode('utf-8'))
+
+
 
 
 @click.command()
--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -5,6 +5,8 @@ import json
 import time
 import click
 import mapreduce.utils
+import socket
+import threading
 
 
 # Configure logging
@@ -25,17 +27,89 @@ class Worker:
         )
 
         # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register_ack",
-            "worker_host": "localhost",
-            "worker_port": 6001,
-        }
-        LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
+        # message_dict = {
+        #     "message_type": "register_ack",
+        #     "worker_host": "localhost",
+        #     "worker_port": 6001,
+        # }
+        # LOGGER.debug("TCP recv\n%s", json.dumps(message_dict, indent=2))
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        LOGGER.debug("IMPLEMENT ME!")
-        time.sleep(120)
+        #TODO:
+        self.host = host
+        self.port = port
+        self.manager_host = manager_host
+        self.manager_port = manager_port
+        self.signals = {"shutdown": False}
+
+
+        thread = threading.Thread(target = self.worker_tcp_server)
+        thread.start()
+        self.worker_tcp_server()
+        self.worker_tcp_client()
+
+
+    def worker_tcp_server(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            # Bind the socket to the server
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.host, self.port))
+            sock.listen()
+            sock.settimeout(1)
+
+            while not self.signals["shutdown"] :
+                # Wait for a connection for 1s.  The socket library avoids consuming
+                # CPU while waiting for a connection.
+                try:
+                    clientsocket, address = sock.accept()
+                except socket.timeout:
+                    continue
+                LOGGER.info("Connection from", address[0])
+
+                
+                clientsocket.settimeout(1)
+
+                with clientsocket:
+                    message_chunks = []
+                    while True:
+                        try:
+                            data = clientsocket.recv(4096)
+                        except socket.timeout:
+                            continue
+                        if not data:
+                            break
+                        message_chunks.append(data)
+
+                # Decode list-of-byte-strings to UTF8 and parse JSON data
+                message_bytes = b''.join(message_chunks)
+                message_str = message_bytes.decode("utf-8")
+
+                try:
+                    message_dict = json.loads(message_str)
+                except json.JSONDecodeError:
+                    LOGGER.error("Failed to decode JSON message.")
+                    continue
+                LOGGER.info(message_dict)
+
+                if message_dict["message_type"] == "shutdown" :
+                    # 如果worker is busy， 先完成job再shutdown
+                    self.signals["shutdown"] = True
+                #  else do work
+                    
+
+
+
+    def worker_tcp_client(self):
+        """Send a registration message to the Manager."""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((self.manager_host, self.manager_port))
+            message = json.dumps({      # 需要写 “finish”
+                "message_type": "register",
+                "worker_host": self.host,
+                "worker_port": self.port
+            })
+            sock.sendall(message.encode('utf-8'))
+            LOGGER.info("Sent register message to Manager")
+
 
 
 @click.command()
